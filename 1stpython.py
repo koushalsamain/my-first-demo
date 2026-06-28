df9a0c7cc74d4d0bfd05dc76e369ec55a39610c5
Koushal Samain <koushalsamain@gmail.com>
2026-06-28 15:22:13 +0530

    Add comment on development branch stratergy
    Added a comment about branching strategy.

--- 1stpython.py
+++ 1stpython.py
@@ -1,2 +1,3 @@
 print ("hello world")
 print ("these are first coding lines in the python on github")
+# now i made a another branch from main and named it as developement branch now whenever i want to change i can update im this branch and can PR to add in main branch
